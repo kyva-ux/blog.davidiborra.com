3bdf861ff6c4d4979035fab43141b4ae801050b3
kyva <hey@davidiborra.com>
2020-05-17 21:02:30 +0200

    Added image support to markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,20 @@ module.exports = {
 				path: `${__dirname}/src/`
 			}
 		},
-		`gatsby-transformer-remark`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 800
+						}
+					}
+				]
+			}
+		},
 		`gatsby-plugin-emotion`,
 		{
 			resolve: `gatsby-plugin-manifest`,
--- src/pages/primer-post.md
+++ src/pages/primer-post.md
@@ -4,3 +4,4 @@ date: '2020-05-12'
 ---
 
 Este es el primer post de mi blog.
+![Grupo de música](../images/music.jpg)
